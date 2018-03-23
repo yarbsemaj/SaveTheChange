c56c0accebae34d711449c7d5e7076e416ab32f1
James <yarbsemaj@gmail.com>
2018-03-23 14:28:17 +0000

    1st Try at OAuth

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('callback', 'OauthController@authorise');
+Route::get('callback', 'OauthController@authorise');
