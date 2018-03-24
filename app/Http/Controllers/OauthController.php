65bcabc852cce3bee038a429789e4c5aa2db35ff
James <yarbsemaj@gmail.com>
2018-03-24 09:36:20 +0000

    Added User Table

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\StarlingUser;
+
 class OauthController extends Controller
 {
-    public function authorise()
+    protected $provider;
+
+    public function __construct()
     {
-        $provider = new \League\OAuth2\Client\Provider\GenericProvider([
+        $this->provider = new \League\OAuth2\Client\Provider\GenericProvider([
             'clientId' => env("clientId"),    // The client ID assigned to you by the provider
             'clientSecret' => env("clientSecret"),   // The client password assigned to you by the provider
             'urlAuthorize' => env("authURL"),
@@ -14,37 +18,28 @@ class OauthController extends Controller
             'urlResourceOwnerDetails' => env("apiURL") . '/api/v1/me',
             'redirectUri' => "http://savethechange.yarbsemaj.com/api/callback"
         ]);
+    }
+
+    public function authorise()
+    {
 
 // If we don't have an authorization code then get one
         if (!isset($_GET['code'])) {
-
-            // Fetch the authorization URL from the provider; this returns the
-            // urlAuthorize option and generates and applies any necessary parameters
-            // (e.g. state).
-            $authorizationUrl = $provider->getAuthorizationUrl();
-
-            // Get the state generated for you and store it to the session.
-            session()->put("oauth2state", $provider->getState());
-
-            // Redirect the user to the authorization URL.
+            $authorizationUrl = $this->provider->getAuthorizationUrl();
+            session()->put("oauth2state", $this->provider->getState());
             header('Location: ' . $authorizationUrl);
             exit;
-
 // Check given state against previously stored one to mitigate CSRF attack
-        } elseif (empty($_GET['state']) || (session()->has("oauth2state") && $_GET['state'] !== session()->has("oauth2state"))) {
-
+        } elseif (empty($_GET['state']) || (session()->has("oauth2state") && $_GET['state'] !== session()->get("oauth2state"))) {
             if (session()->has("oauth2state"))
                 session()->remove('oauth2state');
             exit('Invalid state');
-
         } else {
-
             try {
                 // Try to get an access token using the authorization code grant.
-                $accessToken = $provider->getAccessToken('authorization_code', [
+                $accessToken = $this->provider->getAccessToken('authorization_code', [
                     'code' => $_GET['code']
                 ]);
-
                 // We have an access token, which we may use in authenticated
                 // requests against the service provider's API.
                 echo 'Access Token: ' . $accessToken->getToken() . "<br>";
@@ -54,19 +49,17 @@ class OauthController extends Controller
 
                 // Using the access token, we may look up details about the
                 // resource owner.
-                $resourceOwner = $provider->getResourceOwner($accessToken);
+                $resourceOwner = $this->provider->getResourceOwner($accessToken);
 
-                var_export($resourceOwner->toArray());
 
-                // The provider provides a way to get an authenticated API request for
-                // the service, using the access token; it returns an object conforming
-                // to Psr\Http\Message\RequestInterface.
-                //$request = $provider->getAuthenticatedRequest(
-                //    'GET',
-                //    'http://brentertainment.com/oauth2/lockdin/resource',
-                //    $accessToken
-                //);
+                $user = StarlingUser::firstOrCreate(
+                    ["customer_uid" => $resourceOwner->toArray()['customerUid']],
+                    [
+                        "access_token" => $accessToken->getToken(),
+                        "refresh_token" => $accessToken->getRefreshToken(),
+                        "expires_at" => $accessToken->getToken()]);
 
+                session()->put("user_id", $user->id);
             } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
 
                 print_r($e->getResponseBody());
