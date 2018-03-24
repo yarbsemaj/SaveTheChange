960b78aee2d0890d65929c1970a9da059203fc7c
James <yarbsemaj@gmail.com>
2018-03-24 12:02:12 +0000

    Added User Table

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -14,7 +14,6 @@
             <li><a href="{{ route('login') }}">{{_t("Login")}}</a></li>
             <li><a href="{{ route('register') }}">{{_t("Register")}}</a></li>
         @else
-            @endif
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                    aria-expanded="false" aria-haspopup="true">
@@ -34,6 +33,6 @@
                     </li>
                 </ul>
             </li>
-        @endguest
+        @endif
     </ul>
 </div>
