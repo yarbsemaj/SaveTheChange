77d8d2daf2f97d1857f1438756459a0d849622fa
James <yarbsemaj@gmail.com>
2018-03-24 11:16:38 +0000

    Added User Table

--- app/StarlingUser.php
+++ app/StarlingUser.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class StarlingUser extends Model
 {
     protected $fillable = ["customer_uid", "access_token", "refresh_token", "expires_at"];
+    protected $appends = array('expires');
 
     public function getExpiresAttribute()
     {
